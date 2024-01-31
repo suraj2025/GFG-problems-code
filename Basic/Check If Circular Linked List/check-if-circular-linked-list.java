982a6be96689c408f36e4820d27904d39d1d09e1
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-01-31 22:53:55 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check If Circular Linked List/check-if-circular-linked-list.java
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinklIst_Circular
+{
+	Node head;
+	
+	
+	public static void main(String args[])
+	{
+	    Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int l = sc.nextInt();
+			
+			sc.nextLine().trim().split(" ");
+			String str[]= sc.nextLine().trim().split(" ");
+            Node head = null, tail = null;
+            head = new Node(Integer.parseInt(str[0]));
+            tail = head;
+            for(int i = 1; i < n ; i++)
+            {
+                tail.next = new Node(Integer.parseInt( str[i]));
+                tail = tail.next;
+            }
+                
+			
+			if(l==1 && n>=1)
+			{
+				Node temp = head;
+				while(temp.next != null)
+				{
+					temp = temp.next; 
+				}
+				temp.next = head;
+			}
+			
+			GfG g = new GfG();
+			boolean b = g.isCircular(head);
+			if(b==true)
+			System.out.println(1);
+			else 
+			System.out.println(0);
+		t--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+/* Structure of LinkedList
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+	// Your code here
+	    if(head.next==null){
+	        return false;
+	    }
+	    Node n1=head;
+	    Node n2=head.next;
+	    while(n2.next!=null&&n2.next.next!=null){
+	        if(n1==n2){
+	            return true;
+	        }
+	        n1=n1.next;
+	        n2=n2.next.next;
+	    }
+	    return false;
+	
+    }
+}
