79aee49e0b89be6884a6b2a83c0002e69582ad40
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-01 13:19:07 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Count Odd Even/count-odd-even.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] S = br.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = Integer.parseInt(S[i]);
+            }
+            Solution obj = new Solution();
+            obj.countOddEven(arr, n);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public void countOddEven(int[] arr, int n)
+    {
+        // Code here
+        int even=0;
+        for(int num:arr){
+            if(num%2==0){
+                even++;
+            }
+        }
+        System.out.println (n-even+" "+even);
+    }
+}
