f5edb42d9e929d50a45c55c08995831e91385254
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-08 15:12:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Given a linked list of 0s, 1s and 2s, sort it./given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node head,PrintWriter out)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        // add your code here
+        if(head.next==null){
+            return head;
+        }
+         ArrayList<Integer> al = new ArrayList<>();
+        
+        Node temp = head;
+        while(temp != null){
+            al.add(temp.data);
+            temp = temp.next;
+        }
+        
+        Collections.sort(al);
+        
+        Node ansHead = new Node(al.get(0));
+        Node t = ansHead;
+        for(int i=1; i<al.size(); i++){
+            Node cur = new Node(al.get(i));
+            t.next = cur;
+            t = cur;
+        }
+        
+        return ansHead;
+    }
+}
+
+
