d31ad26da9e78d784b975fe2d60713bcf8923615
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-10 10:16:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head1, int key) {
+        // Add your code here.
+        Node n1=head1;
+        Node n2=new Node(key);
+        if(head1.data>=key){
+            n2.next=head1;
+            head1=n2;
+            return head1;
+        }
+        while(n1!=null){
+            if(n1.next==null){
+                n1.next=n2;
+                return head1;
+            }
+            else if(n1.next.data>=key){
+                n2.next=n1.next;
+                n1.next=n2;
+                break;
+            }
+            n1=n1.next;
+        }
+        return head1;
+        
+        
+    }
+}
