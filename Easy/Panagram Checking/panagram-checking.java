b6d02695af1612eab53802f7158a93af34103b50
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-11 14:38:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Panagram Checking/panagram-checking.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for JAVA
+
+class Solution
+{
+    //Function to check if a string is Pangram or not.
+    public static boolean checkPangram  (String str) {
+        // your code here
+        str=str.toLowerCase();
+        boolean check[]=new boolean[26];
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)-'a'>=0&&str.charAt(i)-'a'<26){
+            check[str.charAt(i)-'a']=true;}
+        }
+        for(int i=0;i<26;i++){
+            if(!check[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	  int t=Integer.parseInt(br.readLine());
+	  while(t-->0)
+	  {
+	    String s=br.readLine().trim();
+	    
+	    System.out.println(new Solution().checkPangram (s)==true?1:0);
+	  }
+	  
+	 }
+}
+// } Driver Code Ends
