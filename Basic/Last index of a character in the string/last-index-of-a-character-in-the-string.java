077af5135fe7156273b032907d1f99ab5a4bbdf1
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-15 12:31:01 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Last index of a character in the string/last-index-of-a-character-in-the-string.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String s = read.readLine().trim();
+            char c = read.readLine().trim().charAt(0);
+            Solution ob = new Solution();
+            System.out.println(ob.LastIndex(s, c));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int LastIndex(String s, char p){
+        // code here
+        return s.lastIndexOf(p);
+    }
+}
