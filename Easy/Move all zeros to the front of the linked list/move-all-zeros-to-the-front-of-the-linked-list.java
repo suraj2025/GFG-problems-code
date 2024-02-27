67cca1bf67c0e6be57d83aee45063c8f51542178
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-27 19:40:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Move all zeros to the front of the linked list/move-all-zeros-to-the-front-of-the-linked-list.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+class Zeroes{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            Node head=null;
+            Node curr=null;
+            while(n-->0){
+                int a=sc.nextInt();
+                if(head==null){
+                    head=new Node(a);
+                    curr=head;
+                }
+                else{
+                    Node temp=new Node(a);
+                    temp.next=null;
+                    curr.next=temp;
+                    curr=temp;
+                }
+            }
+            GfG g=new GfG();
+            head = g.moveZeroes(head);
+            while(head!=null){
+                System.out.print(head.data+" ");
+                head=head.next;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+*/
+
+class GfG{
+    public Node moveZeroes(Node head){
+        //Your Code here.
+        ArrayList<Integer> a=new ArrayList<>();
+        Node temp=head;
+        while(temp!=null){
+            if(temp.data==0){
+                a.add(0,0);
+            }
+            else{
+                a.add(temp.data);
+            }
+            temp=temp.next;
+        }
+        temp=head;
+        for(int num:a){
+            Node n1=new Node(num);
+            temp.next=n1;
+            temp=temp.next;
+        }
+        return head.next;
+        
+    }
+}
