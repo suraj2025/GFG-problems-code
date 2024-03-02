5afcffd4fba585fff4b49b3fd77edb0409218f73
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-03-02 12:35:16 +0530

    Updated Solution - GfG to GitHub

--- Easy/First element to occur k times/first-element-to-occur-k-times.java
+++ Easy/First element to occur k times/first-element-to-occur-k-times.java
@@ -10,13 +10,13 @@ public class GfG {
 		while(t-->0){
 		    String inputLine[] = br.readLine().trim().split(" ");
 		    int n = Integer.parseInt(inputLine[0]);
-		    int m = Integer.parseInt(inputLine[1]);
+		    int k = Integer.parseInt(inputLine[1]);
 		    int[] arr = new int[n];
 		    inputLine = br.readLine().trim().split(" ");
 		    for(int i=0; i<n; i++)arr[i] = Integer.parseInt(inputLine[i]);
 		    
 		    Solution obj = new Solution();
-		    System.out.println(obj.firstElementKTime(arr, n, m));
+		    System.out.println(obj.firstElementKTime(n, k, arr));
 		}
 	}
 }
@@ -26,8 +26,8 @@ public class GfG {
 
 class Solution
 {
-    public int firstElementKTime(int[] arr, int n, int k) { 
-         HashMap<Integer, Integer> map = new HashMap<>();
+    public int firstElementKTime(int n, int k, int[] arr) { 
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : arr) {
             if(map.containsKey(num)){
                int c=map.get(num)+1;
@@ -43,3 +43,5 @@ class Solution
         return -1;
     } 
 }
+
+
