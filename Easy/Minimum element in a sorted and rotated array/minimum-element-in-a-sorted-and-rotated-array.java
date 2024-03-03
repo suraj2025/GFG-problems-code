ff74270a826e6105fb9218daf7dd7e4f844b1048
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-03-03 15:36:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum element in a sorted and rotated array/minimum-element-in-a-sorted-and-rotated-array.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+            int A[] = new int[n];
+            
+            for (int i = 0;i < n;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            System.out.println(sol.findMin(A, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    int findMin(int arr[], int n)
+    {
+        //complete the function here
+        int s=0,e=n-1;
+        int min=Math.min(arr[s++],arr[e--]);
+        while(s<=e){
+            min=Math.min(min,Math.min(arr[s++],arr[e--]));
+            
+        }
+        return min;
+    }
+}
