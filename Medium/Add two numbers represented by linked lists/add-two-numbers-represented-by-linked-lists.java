2e00075fc3ad1f04b81bc3f5e81080170e57a055
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-03-15 18:33:10 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add two numbers represented by linked lists/add-two-numbers-represented-by-linked-lists.java
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+// driver
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GfG{
+    
+    static void printList(Node n){
+        while(n!=null){
+            System.out.print(n.data+" ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        while (T-- > 0) {
+            
+            int n = sc.nextInt();
+            int val = sc.nextInt();
+            
+            Node first = new Node(val);
+            Node tail = first;
+            for(int i=0; i<n-1; i++)
+            {
+                val = sc.nextInt();
+                tail.next = new Node(val);
+                tail = tail.next;
+            }
+            
+            int m = sc.nextInt();
+            val = sc.nextInt();
+            
+            Node second = new Node(val);
+            tail = second;
+            for(int i=0; i<m-1; i++)
+            {
+                val = sc.nextInt();
+                tail.next = new Node(val);
+                tail = tail.next;
+            }
+            
+            Solution g = new Solution();
+            Node res = g.addTwoLists(first, second);
+            printList(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* node for linked list
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+*/
+
+class Solution{
+    //Function to add two numbers represented by linked list.
+    static Node addTwoLists(Node first, Node second){
+        // code here
+        // return head of sum list
+           first = reverseList(first);
+        second = reverseList(second);
+        Node dummy = new Node(0);
+        Node temp = dummy;
+        int carry = 0;
+        while(first != null || second != null || carry == 1){
+            int sum = 0;
+            if(first != null){
+                sum += first.data;
+                first = first.next;
+            }
+            if(second != null){
+                sum += second.data;
+                second = second.next;
+            }
+            
+            sum += carry;
+            carry = sum / 10;
+            Node noo = new Node(sum % 10);
+            temp.next = noo;
+            temp = temp.next;
+        }
+        dummy = reverseList(dummy.next);
+        return dummy;
+    }
+    
+    
+    public static Node reverseList(Node head)
+    {
+        
+        if(head == null || head.next == null) return head;
+        Node newHead = reverseList(head.next);
+        Node front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
+    
+}
