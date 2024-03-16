a75b7a0981933d506285f6024fc3f79234c61b53
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-03-16 16:47:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count Inversions/count-inversions.java
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Sorting
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        long t = sc.nextLong();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+            long arr[] = new long[(int)n];
+            
+            for(long i = 0; i < n; i++)
+             arr[(int)i] = sc.nextLong();
+             
+            System.out.println(new Solution().inversionCount(arr, n));
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    //Function to count inversions in the array.
+     private static long mergeAndCount(long[] arr, int l,
+                                     int m, int r)
+    {
+
+        // Left subarray
+        long[] left = Arrays.copyOfRange(arr, l, m + 1);
+
+        // Right subarray
+        long[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l;
+        long swaps = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+        while (i < left.length)
+            arr[k++] = left[i++];
+        while (j < right.length)
+            arr[k++] = right[j++];
+        return swaps;
+    }
+
+    // Merge sort function
+    private static long mergeSortAndCount(long[] arr, int l,
+                                         int r)
+    {
+
+        // Keeps track of the inversion count at a
+        // particular node of the recursion tree
+        long count = 0;
+
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            // Total inversion count = left subarray count
+            // + right subarray count + merge count
+
+            // Left subarray count
+            count += mergeSortAndCount(arr, l, m);
+
+            // Right subarray count
+            count += mergeSortAndCount(arr, m + 1, r);
+
+            // Merge count
+            count += mergeAndCount(arr, l, m, r);
+        }
+
+        return count;
+    }
+    static long inversionCount(long arr[], long N)
+    {
+        // Your Code Here
+        long res=mergeSortAndCount(arr,0,(int)N-1);
+        return res;
+        
+    }
+}
