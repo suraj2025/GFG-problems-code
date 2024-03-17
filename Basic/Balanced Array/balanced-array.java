28c2db7fa679293afa78f62dbd619fedf8186a63
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-03-17 23:57:24 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Balanced Array/balanced-array.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    long minValueToBalance(long a[] ,int n)
+    {
+        int i=0;int j=n-1;
+        long res=0;
+        while(i<j){
+            res+=a[i++]-a[j--];
+        }
+        res=(long)Math.abs(res);
+        return res;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main (String[] args) throws IOException{
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while(testcases-- > 0){
+            int n=Integer.parseInt(br.readLine());
+
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[]=new long[n];
+            for(int  i=0;i<n;i++)
+            {
+                a[i]=Long.parseLong(a1[i]);
+            }
+            Solution ob=new Solution();
+            
+            long ans=ob.minValueToBalance(a,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
