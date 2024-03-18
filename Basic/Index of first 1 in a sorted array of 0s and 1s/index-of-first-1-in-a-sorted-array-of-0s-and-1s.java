e439706fa9e0975d671a59f17e9a2dcf22fa496d
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-03-18 12:24:00 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Index of first 1 in a sorted array of 0s and 1s/index-of-first-1-in-a-sorted-array-of-0s-and-1s.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.firstIndex(a, n));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public long firstIndex(long arr[], long n)
+    {
+        // Your code goes here
+        int l=0,h=(int)n-1;
+        int mid=(l+h)/2;
+        if(arr[mid]==1){
+            h=mid;
+        }
+        else{
+            l=mid+1;
+        }
+        for(int i=l;i<=h;i++){
+            if(arr[i]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
