4b5076d4a31f4419d8c7b59755743556828f85e5
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-04-22 14:22:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Row with minimum number of 1's/row-with-minimum-number-of-1s.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s[] = read.readLine().split(" ");
+            int N = Integer.parseInt(s[0]);
+            int M = Integer.parseInt(s[1]);
+            int A[][] = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                String S[] = read.readLine().split(" ");
+                for (int j = 0; j < M; j++) {
+                    A[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.minRow(N, M, A));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minRow(int n, int m, int a[][]) {
+        // code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int min=m;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<m;j++){
+                if(a[i][j]==1) count++;
+            }
+            min=Math.min(min,count);
+            map.put(i+1,count);
+        }
+        int res=0;
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            if(e.getValue()==min){
+                res=e.getKey();
+                break;
+            }
+        }
+        // System.out.println(map);
+        // System.out.println(min);
+        return res;
+    }
+};
