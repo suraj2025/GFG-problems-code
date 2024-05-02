d0e3a4d268b5a109039efe50ea27dc0cb31ff7ae
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-05-03 00:11:31 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/k largest elements/k-largest-elements.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] nums, int n, int k) {
+        // code here
+        PriorityQueue<Integer> queue=new PriorityQueue<>(Collections.reverseOrder());
+        for(int num:nums){
+            queue.add(num);
+        }
+        int j=0;
+        int res[]=new int[k];
+        for(int i=0;i<k;i++) {
+            res[j++]=queue.poll();
+        }
+        
+        return res;
+    }
+}
