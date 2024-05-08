cf0c3f424385826f705d4bffa71baba47181a25c
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-05-08 22:46:44 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Reverse sub array/reverse-sub-array.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            inputLine = br.readLine().trim().split(" ");
+            int l = Integer.parseInt(inputLine[0]);
+            int r = Integer.parseInt(inputLine[1]);
+
+            new Solution().reverseSubArray(arr, n, l, r);
+            for (int i = 0; i < n; i++) {
+                out.print(arr[i] + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    void reverseSubArray(int arr[], int n, int l, int r) {
+        // code here
+        l=l-1;
+        r=r-1;
+        while(l<r){
+            int temp=arr[l];
+            arr[l++]=arr[r];
+            arr[r--]=temp;
+        }
+    }
+}
