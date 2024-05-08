178385896f421a015a5dcc6f0e8fc20bf4d51cec
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-05-08 23:04:41 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Minimum Difference among K/minimum-difference-among-k.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    long minDiff(long a[] ,int N,int K)
+    {
+        Arrays.sort(a);
+        long res=a[K-1]-a[0];
+        for(int i=1;i<N-K+1;i++){
+            res=Math.min(res,a[K-1+i]-a[i]);
+        }
+        return res;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main (String[] args) throws IOException{
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while(testcases-- > 0){
+//            int n=Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] a2 = line.trim().split("\\s+");
+            int n =Integer.parseInt(a2[0]);
+            int k =Integer.parseInt(a2[1]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[]=new long[n];
+            for(int  i=0;i<n;i++)
+            {
+                a[i]=Long.parseLong(a1[i]);
+            }
+            Solution ob=new Solution();
+            //ArrayList<Long> ans=ob.smallestDifferenceTriplet(a,b,c,n);
+            long ans=ob.minDiff(a,n,k);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+
+
+
+// } Driver Code Ends
