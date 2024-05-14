98d214b357e8471adb422d89f29d3b72dc650fc1
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-05-15 00:47:12 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort/merge-sort.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Merge_Sort
+{
+    //method to print the elements of the array
+	static void printArray(int arr[])
+    {
+        StringBuffer sb=new StringBuffer("");
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            sb.append(arr[i]+" ");
+        System.out.println(sb.toString());
+    }
+
+    
+
+	public static void main(String args[])
+	{
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int T = sc.nextInt();
+		while(T>0)
+		{
+		    //taking elements count
+			int n = sc.nextInt();
+			
+			//creating an object of class Merge_Sort
+			Merge_Sort ms = new Merge_Sort();
+			
+			//creating an array of size n
+			int arr[] = new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+            
+			Solution g = new Solution();
+			
+			//calling the method mergeSort
+			g.mergeSort(arr,0,arr.length-1);
+
+            //calling the method printArray
+			ms.printArray(arr);
+		T--;
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+         ArrayList<Integer> res=new ArrayList<>();
+         int p1=l;
+         int p2=m+1;
+         
+         while(p1<=m&& p2<=r){
+             if(arr[p1]<=arr[p2]){
+                 res.add(arr[p1]);
+                 p1++;
+             }
+             else if(arr[p1]>arr[p2]){
+                 res.add(arr[p2]);
+                 p2++;
+             }
+         }
+         while(p1<=m){
+             res.add(arr[p1++]);
+         }
+         while(p2<=r){
+             res.add(arr[p2++]);
+         }
+         
+         for (int i = l; i <= r; i++) {
+            arr[i] = res.get(i - l);
+        }
+    }
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if(l==r) return;
+        int mid=(l+r)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
+    }
+}
