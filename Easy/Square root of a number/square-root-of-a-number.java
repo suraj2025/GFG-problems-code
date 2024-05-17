dce0495aa89aaaab3d8c36779c0e6365af1440b0
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-05-17 12:18:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Square root of a number/square-root-of-a-number.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class SquartRoot
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			long a = sc.nextInt();
+			Solution obj = new Solution();
+			System.out.println(obj.floorSqrt(a));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		// Your code here
+		long l=1,h=x;
+		while(l<=h){
+		    long mid=l+(h-l)/2;
+		    if(mid*mid<=x) l=mid+1;
+		    else h=mid-1;
+		}
+		return h;
+	 }
+}
