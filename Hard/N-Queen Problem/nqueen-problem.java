e2b1d40b8163140a48cc062b3af0e9e5a323761b
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-06-15 14:55:03 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/N-Queen Problem/nqueen-problem.java
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0) {
+            int n = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> ans = ob.nQueen(n);
+            if(ans.size() == 0)
+                System.out.println("-1");
+            else {
+                for(int i = 0;i < ans.size();i++){
+                    System.out.print("[");
+                    for(int j = 0;j < ans.get(i).size();j++)
+                        System.out.print(ans.get(i).get(j) + " ");
+                    System.out.print("] ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static boolean validate(char[][] board, int row, int col) {
+        int duprow = row;
+        int dupcol = col;
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0 && row < board.length) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+            row++;
+        }
+        return true;
+    }
+
+    static void dfs(int col, char[][] board, ArrayList < ArrayList < Integer >> res) {
+        if (col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (validate(board, row, col)) {
+                board[row][col] = 'Q';
+                dfs(col + 1, board, res);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+    static ArrayList < Integer > construct(char[][] board) {
+        ArrayList < Integer> res = new ArrayList < Integer > ();
+        for (int i = 0; i < board[0].length; i++) {
+            for(int j=0;j<board.length;j++){
+                if(board[j][i]=='Q'){
+                    res.add(j+1);
+                }
+            }
+            
+        }
+        return res;
+    } 
+    static ArrayList<ArrayList<Integer>> nQueen(int n) {
+        // code here
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+       ArrayList < ArrayList < Integer >> res = new ArrayList <> ();
+        dfs(0, board, res);
+        return res;
+    }
+}
