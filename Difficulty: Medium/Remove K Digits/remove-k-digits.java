595af84670ac96ad21df4de4d31fc004fab6f5ae
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-06-21 16:01:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove K Digits/remove-k-digits.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String S = sc.next();
+            int K = sc.nextInt();
+            Solution obj = new Solution();
+            System.out.println(obj.removeKdigits(S, K));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        // code here
+        Stack<Character> stack = new Stack<>();
+        
+        for (char digit : num.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peek() > digit) {
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+        
+        // Remove remaining k digits from the end of the stack
+        while (k > 0 && !stack.isEmpty()) {
+            stack.pop();
+            k--;
+        }
+        
+        // Construct the resulting string from the stack
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse(); // Reverse to get the correct order
+        
+        // Remove leading zeros
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        
+        // Handle edge case where result might be empty
+        return sb.length() > 0 ? sb.toString() : "0";
+    }
+}
