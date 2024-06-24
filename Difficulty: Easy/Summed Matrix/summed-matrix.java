9c5fc1e0445692385b7bd944ded674f5b4779e38
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-06-24 22:13:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+
+            long n = Long.parseLong(S[0]);
+            long q = Long.parseLong(S[1]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.sumMatrix(n, q));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static long sumMatrix(long n, long q) {
+        // code here
+        if(q>2*n) return 0;
+        if(q>=n+1){
+        long res=(n-q+n+1);
+        return res;}
+        else{
+            
+        long res=(n-n-1+q);
+        return res;
+        }
+    }
+}
