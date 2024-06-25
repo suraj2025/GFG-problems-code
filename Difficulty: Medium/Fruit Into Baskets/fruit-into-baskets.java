10b9331885a9d557b3f2de116718c86eb98d8ffe
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-06-25 22:52:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fruit Into Baskets/fruit-into-baskets.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int N;
+            N = sc.nextInt();
+
+            int[] fruits = new int[N];
+            for (int i = 0; i < N; i++) fruits[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            int res = obj.totalFruits(N, fruits);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int totalFruits(int N, int[] fruits) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int l = 0;
+        int maxLen = 0;
+
+        for (int r = 0; r < N; r++) {
+            map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
+
+            while (map.size() > 2) {
+                map.put(fruits[l], map.get(fruits[l]) - 1);
+                if (map.get(fruits[l]) == 0) {
+                    map.remove(fruits[l]);
+                }
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+}
