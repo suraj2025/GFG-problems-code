9b36a4e72a7808828d4ff2cd4fff2cef72d477ea
Suraj Kumar <sk5195609@gmail.com>
2025-01-21 11:13:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
@@ -23,7 +23,9 @@ class GFG {
             Solution obj = new Solution();
             long res = obj.solve(N, M, grid);
             System.out.println(res);
-        }
+        
+System.out.println("~");
+}
     }
 }
 // } Driver Code Ends
@@ -32,60 +34,63 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    // int helper(int i,int j1,int j2,int n, int m, int grid[][]){
-    //     if(j1<0||j1>=m||j2<0||j2>=m){
-    //         return (int)-1e9;
-    //     }
-    //     if(i==n-1){
-    //         if(j1==j2) return grid[i][j1];
-    //         else return grid[i][j1]+grid[i][j2];
-    //     }
-    //     int maxi=0;
-    //     for(int p1=-1;p1<=1;p1++){
-    //         for(int p2=-1;p2<=1;p2++){
-    //             if(j1==j2){
-    //                 maxi=Math.max(maxi,grid[i][j1]+helper(i+1,j1+p1,j2+p2,n,m,grid));
-    //             }
-    //             else{
-    //                 maxi=Math.max(maxi,grid[i][j1]+grid[i][j2]+helper(i+1,j1+p1,j2+p2,n,m,grid));
-    //             }
-    //         }
-    //     }
-    //     return maxi;
-    // }
-    public int solve(int n, int m, int grid[][]) {
-        // Code here
-        int dp[][][]=new int[n][m][m];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<m;j++){
-                if(i==j){
-                    dp[n-1][i][j]=grid[n-1][i];
-                }
-                else{
-                    dp[n-1][i][j]=grid[n-1][j]+grid[n-1][i];
-                }
+    int fun(int i,int j1,int j2,int n,int m,int grid[][],int dp[][][]){
+        if(j1<0||j1>=m||j2<0||j2>=m) return Integer.MIN_VALUE;
+        if(i==n-1){
+            if(j1==j2) return grid[i][j1];
+            return grid[i][j1]+grid[i][j2];
+        }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        int maxi=Integer.MIN_VALUE;
+        for(int d1=-1;d1<=1;d1++){
+            for(int d2=-1;d2<=1;d2++){
+                int ans=fun(i+1,j1+d1,j2+d2,n,m,grid,dp);
+                maxi=Math.max(maxi,ans);
             }
         }
+        if(j1==j2)  maxi+=grid[i][j1];
+        else maxi+=grid[i][j1]+grid[i][j2];
+        return dp[i][j1][j2]=maxi;
+    }
+    public int solve(int n, int m, int grid[][]) {
+        int dp[][][]=new int[n][m][m];
+        for(int j1=0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1==j2) dp[n-1][j1][j2]=grid[n-1][j1];
+               else dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+            }
+        }
+        
         for(int i=n-2;i>=0;i--){
             for(int j1=0;j1<m;j1++){
                 for(int j2=0;j2<m;j2++){
-                    int maxi=(int)-1e8;
-                    for(int p1=-1;p1<=1;p1++){
-                        for(int p2=-1;p2<=1;p2++){
-                            int value = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
-                            int nj1 = j1 + p1;
-                            int nj2 = j2 + p2;
-                            if (nj1 >= 0 && nj1 < m && nj2 >= 0 && nj2 < m) {
-                                value+=dp[i+1][nj1][nj2];
+                    int maxi=Integer.MIN_VALUE;
+                    for(int d1=-1;d1<=1;d1++){
+                        for(int d2=-1;d2<=1;d2++){
+                            
+                            if(j1+d1>=0&&j1+d1<m&&j2+d2>=0&&j2+d2<m){
+                                int ans=dp[i+1][j1+d1][j2+d2];
+                                maxi=Math.max(maxi,ans);
+                                
                             }
-                            else value=(int)-1e8;
-                            maxi = Math.max(maxi, value );
+                        //     int newJ1 = j1 + d1;
+                        //     int newJ2 = j2 + d2;
+
+                        // // Check if the new positions are within bounds
+                        //     if (newJ1 >= 0 && newJ1 < m && newJ2 >= 0 && newJ2 < m) {
+                        //         int ans = dp[i + 1][newJ1][newJ2];
+                        //         maxi = Math.max(maxi, ans);
+                        //     }
                         }
                     }
+                    if(j1==j2)  maxi+=grid[i][j1];
+                    else maxi+=grid[i][j1]+grid[i][j2];
                     dp[i][j1][j2]=maxi;
                 }
             }
         }
+        
+        // Code here
         return dp[0][0][m-1];
     }
 }
